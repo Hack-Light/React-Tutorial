028158c6b46973cacfe89294db0c27f4e0616af2
Hack-Light <onohsomtochukwubasil@gmail.com>
2021-01-03 23:25:25 +0100

    Setup for advanced tutorial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+
+// import App from "./components/app";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-import App from "./components/app";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

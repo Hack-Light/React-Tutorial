d8b996058687373e3c44cd27bb5e5237a0aa30f8
Hack-Light <onohsomtochukwubasil@gmail.com>
2021-01-04 18:42:18 +0100

    i now have the basic understanding of how useState works

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+/* eslint-disable eqeqeq */
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Hello World");
+
+  const handleClick = () => {
+    text == "Hello World" ? setText("Random Text") : setText("Hello World");
+  };
+
+  return (
+    <>
+      <h2>useState basic example</h2>
+      <h2>{text}</h2>
+
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Text
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;

ebe2c304a0f8c03fbdf7314ae4eeb1c22a5b83ac
Hack-Light <onohsomtochukwubasil@gmail.com>
2021-01-04 19:31:45 +0100

    I can now use the useState to manipulate arrays

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { data } from '../../../data';
+/* eslint-disable eqeqeq */
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem = id => {
+    let newPeople = people.filter(person => person.id != id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <>
+      <h2>useState array example</h2>
+      {people.map(({ id, name }) => {
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>Clear</button>
+          </div>
+        );
+      })}
+      {people.length != 0 && (
+        <button className="btn" onClick={() => setPeople([])}>
+          Clear People
+        </button>
+      )}
+    </>
+  );
 };
 
 export default UseStateArray;

61a68d6ea4302a5a223c4db2dd44bd0a643fd9a6
Hack-Light <onohsomtochukwubasil@gmail.com>
2021-01-06 00:16:07 +0100

    I now know how to clean up useEffect

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("effect");
+    window.addEventListener("resize", checkSize);
+
+    /* return () => {
+      window.removeEventListener("resize", checkSize);
+      console.log("remove");
+    }; */
+  }, []);
+  console.log("render");
+  return (
+    <>
+      <h1>Window</h1>
+      <h3>{size} px</h3>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
